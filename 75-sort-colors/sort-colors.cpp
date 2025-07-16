3eeacccfd0d136b4948f1141475e10dfdbaa42f4
Sakshi Haldkar <haldkarsakshi88@gmail.com>
2025-07-16 20:29:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (97.43%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,27 +1,51 @@
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int n = nums.size();
+//         int count0 = 0,count1= 0, count2= 0;
+
+//         //O(n)
+//         for(int i = 0; i<n; i++){
+//             if(nums[i] == 0) count0++;
+//             else if (nums[i] == 1) count1++;
+//             else count2++;
+//         }
+
+//         //O(n)
+//         int idx = 0;
+//         for(int i =0; i<count0; i++){
+//             nums[idx++] = 0;
+//         }
+//         for(int i= 0; i<count1; i++){
+//             nums[idx++] = 1;
+
+//         }
+//         for(int i=0; i<count2; i++){
+//             nums[idx++] = 2;
+//         }
+//     }
+// };
+
+
+
+//****Dutch national flag algo
+
 class Solution {
-public:
-    void sortColors(vector<int>& nums) {
+    public:
+    void sortColors (vector<int> & nums) {//O(n)
         int n = nums.size();
-        int count0 = 0,count1= 0, count2= 0;
+        int low = 0, mid = 0, high = n-1;
 
-        //O(n)
-        for(int i = 0; i<n; i++){
-            if(nums[i] == 0) count0++;
-            else if (nums[i] == 1) count1++;
-            else count2++;
-        }
-
-        //O(n)
-        int idx = 0;
-        for(int i =0; i<count0; i++){
-            nums[idx++] = 0;
-        }
-        for(int i= 0; i<count1; i++){
-            nums[idx++] = 1;
-
-        }
-        for(int i=0; i<count2; i++){
-            nums[idx++] = 2;
+        while(mid <= high){
+            if(nums[mid] == 0) {
+                swap(nums[low] , nums[mid]);
+                mid++; low++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else {
+                swap(nums[high], nums[mid]);
+                high--;        
+}
         }
     }
-};
+    };
