49808ae26a34879785970ac85911a9953bd2a877
Sakshi Haldkar <haldkarsakshi88@gmail.com>
2025-08-05 21:00:28 +0530

    Time: 50 ms (52.82%) | Memory: 46.8 MB (40.02%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
+    int subarraySum(vector<int>& arr, int k) {
+        /*int n = nums.size();
         int count = 0;
 
         for(int i =0; i<n; i++){//strting point
@@ -12,5 +12,32 @@ public:
             }
         }
         return count;
+    }
+};*/
+
+int n = arr.size();
+int count = 0;
+vector<int> prefixSum(n,0);
+
+prefixSum[0] = arr[0];
+for(int i =1; i<n; i++){
+    prefixSum[i] = prefixSum[i-1] + arr[i];
+}
+
+unordered_map<int, int> m;//PS,freq
+for(int j = 0; j<n; j++){
+    if(prefixSum[j] == k) count++;
+
+    int val = prefixSum[j] - k;
+    if(m.find(val) != m.end()){
+        count += m[val];
+    }
+
+    if(m.find(prefixSum[j]) == m.end()){
+        m[prefixSum[j]]=0;
+    }
+    m[prefixSum[j]]++;
+}
+return count;
     }
 };
